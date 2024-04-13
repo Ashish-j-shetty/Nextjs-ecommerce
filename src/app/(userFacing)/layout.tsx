abc7957fa26e7e9d3e7aaca6d8feaf76cb463d11
Ashish-J-Shetty <ashishshetty2612@gmail.com>
2024-04-13 16:22:03 +0530

    feat: product pages and caching

---
+++ src/app/(userFacing)/layout.tsx
@@ -0,0 +1,18 @@
+import Nav, { NavLink } from "@/components/Nav";
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href={"/"}>Home</NavLink>
+        <NavLink href={"/products"}>Product</NavLink>
+        <NavLink href={"/orders"}>My Orders</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+}
